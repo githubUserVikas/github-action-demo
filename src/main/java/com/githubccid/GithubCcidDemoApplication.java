f10400175608e49f01f724bf6b19267ffc64c816
Vikas <vikas.verma0442@gmail.com>
2022-05-22 20:27:46 +0530

    1: Github cicd

---
+++ src/main/java/com/githubccid/GithubCcidDemoApplication.java
@@ -0,0 +1,13 @@
+package com.githubccid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCcidDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCcidDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubccid/GithubCcidDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubccid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCcidDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
