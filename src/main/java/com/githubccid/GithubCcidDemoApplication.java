18fd92adfe6cef356ca5ab0878e8c56446efca8c
Vikas <vikas.verma0442@gmail.com>
2022-05-23 00:38:58 +0530

    Controller file modified.

--- src/main/java/com/githubccid/GithubCcidDemoApplication.java
+++ src/main/java/com/githubccid/GithubCcidDemoApplication.java
@@ -2,10 +2,18 @@ package com.githubccid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubCcidDemoApplication {
 
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to Github CICD Configuration";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCcidDemoApplication.class, args);
 	}
